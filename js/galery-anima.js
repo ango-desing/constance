444c841e2e991d7e4e49340cc191c26320611d79
ango-desing <74431868+ango-desing@users.noreply.github.com>
2021-06-09 16:00:59 -0600

    wer

--- js/galery-anima.js
+++ js/galery-anima.js
@@ -1,41 +1,178 @@
-function animateFrom(elem, direction) {
-   direction = direction || 1;
-   var x = 0,
-       y = direction * 100;
-   if(elem.classList.contains("gs_reveal_fromLeft")) {
-     x = -100;
-     y = 0;
-   } else if (elem.classList.contains("gs_reveal_fromRight")) {
-     x = 100;
-     y = 0;
-   }
-   elem.style.transform = "translate(" + x + "px, " + y + "px)";
-   elem.style.opacity = "0";
-   gsap.fromTo(elem, {x: x, y: y, autoAlpha: 0}, {
-     duration: 1.25, 
-     x: 0,
-     y: 0, 
-     autoAlpha: 1, 
-     ease: "expo", 
-     overwrite: "auto"
-   });
- }
- 
- function hide(elem) {
-   gsap.set(elem, {autoAlpha: 0});
- }
- 
- document.addEventListener("DOMContentLoaded", function() {
-   gsap.registerPlugin(ScrollTrigger);
-   
-   gsap.utils.toArray(".gs_reveal").forEach(function(elem) {
-     hide(elem);
-     ScrollTrigger.create({
-       trigger: elem,
-       onEnter: function() { animateFrom(elem) }, 
-       onEnterBack: function() { animateFrom(elem, -1) },
-       onLeave: function() { hide(elem) }
-     });
-   });
- });
- 
+/*<![CDATA[*/
+(function () {
+  var scriptURL = 'https://sdks.shopifycdn.com/buy-button/latest/buy-button-storefront.min.js';
+  if (window.ShopifyBuy) {
+    if (window.ShopifyBuy.UI) {
+      ShopifyBuyInit();
+    } else {
+      loadScript();
+    }
+  } else {
+    loadScript();
+  }
+  function loadScript() {
+    var script = document.createElement('script');
+    script.async = true;
+    script.src = scriptURL;
+    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(script);
+    script.onload = ShopifyBuyInit;
+  }
+  function ShopifyBuyInit() {
+    var client = ShopifyBuy.buildClient({
+      domain: 'contance.myshopify.com',
+      storefrontAccessToken: '8977acf40a4b1e31fde8fc9017bf387d',
+    });
+    ShopifyBuy.UI.onReady(client).then(function (ui) {
+      ui.createComponent('product', {
+        id: '6810538115237',
+        node: document.getElementById('product-component-1623274199115'),
+        moneyFormat: '%24%7B%7Bamount%7D%7D',
+        options: {
+  "product": {
+    "styles": {
+      "product": {
+        "@media (min-width: 601px)": {
+          "max-width": "100%",
+          "margin-left": "0",
+          "margin-bottom": "50px"
+        },
+        "text-align": "left"
+      },
+      "title": {
+        "font-size": "26px"
+      },
+      "button": {
+        ":hover": {
+          "background-color": "#000000"
+        },
+        "background-color": "#000000",
+        ":focus": {
+          "background-color": "#000000"
+        },
+        "padding-left": "30px",
+        "padding-right": "30px"
+      },
+      "price": {
+        "font-size": "18px"
+      },
+      "compareAt": {
+        "font-size": "15.299999999999999px"
+      },
+      "unitPrice": {
+        "font-size": "15.299999999999999px"
+      }
+    },
+    "layout": "horizontal",
+    "contents": {
+      "img": false,
+      "imgWithCarousel": true,
+      "button": false,
+      "buttonWithQuantity": true,
+      "description": true
+    },
+    "width": "100%",
+    "text": {
+      "button": "Add to cart"
+    }
+  },
+  "productSet": {
+    "styles": {
+      "products": {
+        "@media (min-width: 601px)": {
+          "margin-left": "-20px"
+        }
+      }
+    }
+  },
+  "modalProduct": {
+    "contents": {
+      "img": false,
+      "imgWithCarousel": true,
+      "button": false,
+      "buttonWithQuantity": true
+    },
+    "styles": {
+      "product": {
+        "@media (min-width: 601px)": {
+          "max-width": "100%",
+          "margin-left": "0px",
+          "margin-bottom": "0px"
+        }
+      },
+      "button": {
+        ":hover": {
+          "background-color": "#000000"
+        },
+        "background-color": "#000000",
+        ":focus": {
+          "background-color": "#000000"
+        },
+        "padding-left": "30px",
+        "padding-right": "30px"
+      },
+      "title": {
+        "font-family": "Helvetica Neue, sans-serif",
+        "font-weight": "bold",
+        "font-size": "26px",
+        "color": "#4c4c4c"
+      },
+      "price": {
+        "font-family": "Helvetica Neue, sans-serif",
+        "font-weight": "normal",
+        "font-size": "18px",
+        "color": "#4c4c4c"
+      },
+      "compareAt": {
+        "font-family": "Helvetica Neue, sans-serif",
+        "font-weight": "normal",
+        "font-size": "15.299999999999999px",
+        "color": "#4c4c4c"
+      },
+      "unitPrice": {
+        "font-family": "Helvetica Neue, sans-serif",
+        "font-weight": "normal",
+        "font-size": "15.299999999999999px",
+        "color": "#4c4c4c"
+      }
+    },
+    "text": {
+      "button": "Add to cart"
+    }
+  },
+  "option": {},
+  "cart": {
+    "styles": {
+      "button": {
+        ":hover": {
+          "background-color": "#000000"
+        },
+        "background-color": "#000000",
+        ":focus": {
+          "background-color": "#000000"
+        }
+      }
+    },
+    "text": {
+      "total": "Subtotal",
+      "button": "Checkout"
+    }
+  },
+  "toggle": {
+    "styles": {
+      "toggle": {
+        "background-color": "#000000",
+        ":hover": {
+          "background-color": "#000000"
+        },
+        ":focus": {
+          "background-color": "#000000"
+        }
+      }
+    }
+  }
+},
+      });
+    });
+  }
+})();
+/*]]>*/
